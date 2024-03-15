7fea2883253973c49dd9d90cbbf4f4d7621bb3ee
AhrensSG <guillermoahrens@gmail.com>
2024-03-15 00:52:27 -0300

    Add temporally contact section

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import FirstSection from "@/components/home/FirstSection";
 import FourthSection from "@/components/home/FourthSection";
 import SecondSection from "@/components/home/SecondSection";
 import ThirdSection from "@/components/home/ThirdSection";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,9 +13,9 @@ export default function Home() {
       <SecondSection />
       <ThirdSection />
       <FourthSection />
-      {/* <section className="w-full h-screen relative flex">
+      <section className="w-full h-screen relative">
         <Image
-          src={"/EndPageJirafe.png"}
+          src={"/ContactSection.svg"}
           fill
           alt="FutureJirafe"
           className="object-cover object-top  transition-opacity opacity-0 duration-500 z-0"
@@ -23,13 +24,8 @@ export default function Home() {
           priority={true}
           quality={100}
         />
-        <div className="w-full h-full flex justify-end items-center z-10">
-          <div className="w-1/2 h-full flex flex-col justify-center items-start">
-            
-          </div>
-        </div>
-      </section> */}
-      <Footer/>
+      </section>
+      <Footer />
     </main>
   );
 }
