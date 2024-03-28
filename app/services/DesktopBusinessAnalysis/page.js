717188aeb496734dd5fd4c3a96d3c110c90c59a6
AhrensSG <guillermoahrens@gmail.com>
2024-03-28 12:10:04 -0300

    Add DesktopTemplate and desktop businessAnalysis page

---
+++ app/services/DesktopBusinessAnalysis/page.js
@@ -0,0 +1,12 @@
+import DesktopTemplate from '@/components/services/DesktopTemplate'
+import React from 'react'
+
+const DesktopBusinessAnalysis = () => {
+  return (
+    <DesktopTemplate>
+      
+    </DesktopTemplate>
+  )
+}
+
+export default DesktopBusinessAnalysis
--- components/services/DesktopTemplate.jsx
+++ components/services/DesktopTemplate.jsx
@@ -2,21 +2,34 @@ import Image from "next/image";
 import React from "react";
 import NavBar from "../navbar/NavBar";
 
-const DesktopTemplate = ({ children }) => {
+const DesktopTemplate = ({ children, media = "/MediaPlayer.svg", icon = "/services/Jirafe4.svg"  }) => {
   return (
     <main className="bg-slate-100 w-full h-full">
-      <section className="bg-[url('/WaveBG.svg')] bg-cover w-full h-full flex flex-col relative">
+      <section className="bg-gradient-to-b from-slate-100 to-[#0853FC] w-full h-[100vh] flex flex-col relative">
         <NavBar />
-        <div className="w-full h-full flex flex-col items-center py-10">
-          <Image
-            src={"/MediaPlayer.svg"}
-            width={800}
-            height={650}
-            alt="MediaPlayer"
-            className="z-10"
-          />
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="relative max-w-[850px] w-full max-h-96 h-full">
+            <Image src={media} fill alt="Mediaplayer" />
+          </div>
         </div>
       </section>
+      <div className="relative max-w-[100vw] w-full h-56">
+        <Image
+          src={"/WaveBG.jpg"}
+          fill
+          priority
+          quality={100}
+          sizes="100vw"
+          className="object-cover object-top"
+        />
+        <Image
+          src={icon}
+          priority
+          width={200}
+          height={200}
+          className="absolute left-[22%] bottom-0"
+        />
+      </div>
     </main>
   );
 };
