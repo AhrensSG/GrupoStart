b695d59465712c158d8b569918cdcf43fb129e1b
Guillermo Ahrens <guillermoahrens@gmail.com>
2026-07-10 18:56:54 +0200

    Cambia email de sugerencias a grupostart.ok@gmail.com

--- components/tools/SuggestModal.jsx
+++ components/tools/SuggestModal.jsx
@@ -15,7 +15,7 @@ export default function SuggestModal({ onClose }) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          to: "guillermoahrens@gmail.com",
+          to: "grupostart.ok@gmail.com",
           subject: "Sugerencia - GrupoStart Tools",
           text: message,
         }),
